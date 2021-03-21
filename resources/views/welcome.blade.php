38ae1098387a4872b3c2bfb53a0b80c674be6e49
Eni <estefanielunardi@users.noreply.github.com>
2021-03-21 21:51:26 +0100

    style: welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,12 +47,12 @@
                 <div class="bg-white shadow-md rounded my-6">
                     <table class="min-w-max w-full table-auto">
                         <thead>
-                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                            <tr class="bg-gray-200 text-gray-600 uppercase text-lg leading-normal">
                                 <th class="py-3 px-6 text-left">User</th>
                                 <th class="py-3 px-6 text-center">Roles</th>
                             </tr>
                         </thead>
-                        <tbody class="text-gray-600 text-sm font-light">
+                        <tbody class="text-gray-600 text-lg font-light">
                             @foreach($users as $user)
                             <tr class="border-b border-gray-200 hover:bg-gray-100">
                                 <td class="py-3 px-6 text-left">
