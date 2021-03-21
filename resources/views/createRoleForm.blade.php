e2b1a0cbb486c84eb1fa51fe3bdbec155bb04146
Eni <estefanielunardi@users.noreply.github.com>
2021-03-21 21:47:05 +0100

    Role: create role form style

--- resources/views/createRoleForm.blade.php
+++ resources/views/createRoleForm.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
+    <header class="relative">
+        <img class="w-full h-48 object-cover lg:h-72" src="https://picsum.photos/600/400/?random">
+    </header>
     <section class="flex flex-col m-10">
-        <h1 class="title pb-8">New Role</h1>
+        <h1 class="title pb-8 text-4xl">New Role</h1>
     </section>
     <section class="container md:container md:mx-auto p-6 flex justify-center">
         <div class="box-border p-4 bg-white h-128 w-96">
@@ -8,11 +11,11 @@
                 @method('POST')
                 @csrf
                 <div class="flex flex-col my-4 text-xl">
-                    <label for="title" class="font-serif">{{ __('Role Title') }}</label>
+                    <label for="title" class="antialiased">{{ __('Role Title') }}</label>
                     <input type="text" id="title" class="w-100 border-solid border-2 rounded shadow-md h-10" name="title" required autocomplete="title" autofocus>
                 </div>
                 <div class="flex justify-center">
-                    <button type="submit" class="bg-blue-400 font-serif text-black text-2xl mt-4 px-12 py-4 rounded-xl shadow-md">{{ __('Add Role') }}</button>
+                    <button type="submit" class="bg-blue-400 antialiased text-black text-2xl mt-4 px-12 py-4 rounded-xl shadow-md">{{ __('Add Role') }}</button>
                 </div>
             </form>
         </div>
